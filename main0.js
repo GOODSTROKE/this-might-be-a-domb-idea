fcbd0f8ced63ec712ef20578229cfeda99f1406a
Raulen A Conway <Harmongroupmedia@gmail.com>
2022-11-14 16:44:59 -0500

    Assignment Update

---
+++ main0.js
@@ -0,0 +1,41 @@
+//Change the font color of the paragraph to light blue.
+
+let para = document.querySelector('p');
+
+para.style.color = "lightblue";
+
+//* Change the font size of the heading to 48px.
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = "48px";
+
+//* Change the text of the paragraph to be the following Hipster Ipsum:
+para.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur necessitatibus quod nesciunt esse, corporis aliquam ullam deleniti dolore neque autem! Nihil tempora impedit earum perferendis pariatur molestiae sequi possimus?"
+
+// para.style.fontFamily;
+
+// * Change the 13th item to be half transparent. (Look this up!)
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = "0.5";
+
+//console.log(#item13)
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = "I say, Hello";
+
+let item16 = document.querySelector('#item-16');
+item16.innerText = "Won't get fooled again.";
+
+
+//* Change the image below the list to be whatever image you want.
+let myImage = document.querySelector ("img")
+myImage.src = "https://img1.wsimg.com/isteam/ip/25cc4b35-502b-40c1-a869-4006148d1cb5/KME%20-%20FETUS%20-%20015.png/:/rs=w:1160,h:387"
+
+//* Change that image's size to be 300 pixels high.
+myImage.style.height = '300px';
+
+
+//* Using HTML, add a new image of your choice below or to the right of the other image and give it a unique id.
+
+//* Now make that image 300 pixels high (with JS)
+
